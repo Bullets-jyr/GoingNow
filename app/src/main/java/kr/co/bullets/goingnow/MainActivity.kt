292d2fc04e5be78b81b4838fbdcf1d0c70811732
Bullets-jyr <jyr@bullets.co.kr>
2023-09-01 23:11:27 +0900

    9. Implement the Splash Screen

--- app/src/main/java/kr/co/bullets/goingnow/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/goingnow/MainActivity.kt
@@ -3,11 +3,13 @@ package kr.co.bullets.goingnow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import kr.co.bullets.goingnow.ui.theme.GoingNowTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             GoingNowTheme {
 
